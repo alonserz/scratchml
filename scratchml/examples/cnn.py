c7b3b20544965f243c0a0bb555b233bd68de8249
alonserz <59419908+alonserz@users.noreply.github.com>
2025-09-15 00:47:02 +0300

    Example update. Add dropout layer

--- scratchml/examples/cnn.py
+++ scratchml/examples/cnn.py
@@ -1,6 +1,5 @@
 import numpy as np
-
-from scratchml.layers import Conv, AvgPooling, ReLU, Flatten
+from scratchml.layers import Conv, AvgPooling, ReLU, Flatten, Dropout
 from sklearn.datasets import fetch_openml
 from sklearn.utils import check_random_state
 
@@ -15,22 +14,17 @@ from sklearn.metrics import accuracy_score
 X_ds, y_ds = fetch_openml("mnist_784", version=1, return_X_y=True, as_frame=False)
 
 
+n_samples = 200
 
-random_state = check_random_state(0)
-permutation = random_state.permutation(X_ds.shape[0])
-X_ds = X_ds[permutation]
-y_ds = y_ds[permutation]
-
-X = X_ds[:100]
-y = y_ds[:100]
+X = X_ds[:n_samples]
+y = y_ds[:n_samples]
 y = np.array(list(map(lambda x: int(x), y)))
-X = X.reshape((X.shape[0], -1))
 y = to_categorical(y)
-X = X.reshape((100, 28, 28))
+X = X.reshape((n_samples, 28, 28))
 
-X = np.expand_dims(X, axis = 1)
+X = np.expand_dims(np.array(X), axis = 1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state= 42)
 
 scaler = STDScaler()
 X_train = scaler.fit_transform(X_train)
@@ -39,33 +33,38 @@ X_test = scaler.transform(X_test)
 model = Sequence(
     Conv(1, 6, 5),
     AvgPooling(2, stride = 2),
+    Dropout(p = 0.1),
     ReLU(),
     Conv(6, 16, 5),
     AvgPooling(2, stride = 2),
+    Dropout(p = 0.1),
     ReLU(),
     Conv(16, 120, 4),
     Flatten(),
     Linear(120, 80),
+    Dropout(p = 0.1),
     ReLU(),
     Linear(80, 10),
     Softmax()
 )
 
-optim = SGD(model, lr = 3e-3, weight_decay = 3e-4)
+optim = Adam(model, lr = 1e-3)
 loss = CrossEntropy()
 
 def train(X, y, n_epochs = 200):
+    model.train()
     for epoch in range(n_epochs):
         print(f"Epoch #{epoch}")
         y_pred = model(X_train)
         _loss = loss(y_train, y_pred)
-        print(np.mean(_loss))
-        print(accuracy_score(np.argmax(y_train, axis = 1), np.argmax(y_pred, axis = 1)))
+        print("Train Loss:", np.mean(_loss))
+        print("Train accuracy: ", accuracy_score(np.argmax(y_train, axis = 1), np.argmax(y_pred, axis = 1)))
         loss_grad = loss.gradient(y_train, y_pred)
         model.backward(loss_grad)
 
-        y_pred = model(X_test)
-        print(accuracy_score(np.argmax(y_test, axis = 1), np.argmax(y_pred, axis = 1)))
+    model.eval()
+    y_pred = model(X_test)
+    print("Test accuracy: ", accuracy_score(np.argmax(y_test, axis = 1), np.argmax(y_pred, axis = 1)))
 
     
-train(X, y, 200)
+train(X, y, 20)
