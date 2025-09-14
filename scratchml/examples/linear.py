47131ff85c2d32af30abab5a690cc039ce84ddd4
alonserz <59419908+alonserz@users.noreply.github.com>
2025-09-14 22:38:00 +0300

    Add dropout layer to example

--- scratchml/examples/linear.py
+++ scratchml/examples/linear.py
@@ -7,17 +7,17 @@ from sklearn.model_selection import train_test_split
 
 from scratchml.optimizer import Adam, RMSProp, SGD
 from scratchml.utils import Sequence, to_categorical
-from scratchml.layers import Linear, ReLU, Softmax
+from scratchml.layers import Linear, ReLU, Softmax, Dropout
 from scratchml.loss import CrossEntropy
 from scratchml.utils import STDScaler
-    
+from sklearn.metrics import accuracy_score
 
 dataset = load_digits()
 X = dataset.data
 y = dataset.target
 _y = to_categorical(y.astype("int"))
 
-X_train, X_test, y_train, y_test = train_test_split(X, _y, test_size=0.4)
+X_train, X_test, y_train, y_test = train_test_split(X, _y, test_size=0.2, random_state =42)
 
 scaler = STDScaler()
 X_train = scaler.fit_transform(X_train)
@@ -25,8 +25,10 @@ X_train = scaler.fit_transform(X_train)
 
 model = Sequence(
     Linear(64, 12),
+    Dropout(p = 0.2),
     ReLU(),
     Linear(12, 12),
+    Dropout(p = 0.2),
     ReLU(),
     Linear(12, 10),
     Softmax(),
@@ -36,8 +38,14 @@ optim = Adam(model, lr = 1e-3)
 loss = CrossEntropy()
 
 for epoch in range(200):
+    model.train()
     y_pred = model(X_train)
     _loss = loss(y_train, y_pred)
     print(np.mean(_loss))
     loss_grad = loss.gradient(y_train, y_pred)
     model.backward(loss_grad)
+    print("Train accuracy: ", accuracy_score(np.argmax(y_train, axis = 1), np.argmax(y_pred, axis = 1)))
+
+    model.eval()
+    y_pred = model(X_test)
+    print("Test accuracy: ", accuracy_score(np.argmax(y_test, axis = 1), np.argmax(y_pred, axis = 1)))
