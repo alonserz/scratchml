558b703dba0d53242b9262bdce2711e478127e96
alonserz <59419908+alonserz@users.noreply.github.com>
2025-09-17 00:31:09 +0300

    Change image resolution form 28x28 to 32x32 using padding = 2. Change model to LeNet5

--- scratchml/examples/cnn.py
+++ scratchml/examples/cnn.py
@@ -24,7 +24,7 @@ X = X.reshape((n_samples, 28, 28))
 
 X = np.expand_dims(np.array(X), axis = 1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state= 42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 42)
 
 scaler = STDScaler()
 X_train = scaler.fit_transform(X_train)
@@ -32,12 +32,12 @@ X_test = scaler.transform(X_test)
 
 model = Sequence(
     Conv(1, 6, 5, padding = 2), # (1x28x28) -> (1x32x32)
-    AvgPooling(2, stride = 2),
+    AvgPooling(2),
     BatchNorm2D(6),
     Dropout(p = 0.1),
     ReLU(),
     Conv(6, 16, 5),
-    AvgPooling(2, stride = 2),
+    AvgPooling(2),
     BatchNorm2D(16),
     Dropout(p = 0.1),
     ReLU(),
@@ -70,4 +70,4 @@ def train(X, y, n_epochs = 200):
     print("Test accuracy: ", accuracy_score(np.argmax(y_test, axis = 1), np.argmax(y_pred, axis = 1)))
 
     
-train(X, y, 30)
+train(X, y, 10)
