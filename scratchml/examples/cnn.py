86c639239b8ce97227ca5ca612629c9581da77bc
alonserz <59419908+alonserz@users.noreply.github.com>
2025-09-16 23:19:05 +0300

    Add padding to first conv layer

--- scratchml/examples/cnn.py
+++ scratchml/examples/cnn.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scratchml.layers import Conv, AvgPooling, ReLU, Flatten, Dropout, BatchNorm
+from scratchml.layers import Conv, AvgPooling, ReLU, Flatten, Dropout, BatchNorm2D
 from sklearn.datasets import fetch_openml
 from sklearn.utils import check_random_state
 
@@ -31,18 +31,18 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 model = Sequence(
-    Conv(1, 6, 5),
+    Conv(1, 6, 5, padding = 2), # (1x28x28) -> (1x32x32)
     AvgPooling(2, stride = 2),
-    BatchNorm(6),
+    BatchNorm2D(6),
     Dropout(p = 0.1),
     ReLU(),
     Conv(6, 16, 5),
     AvgPooling(2, stride = 2),
-    BatchNorm(16),
+    BatchNorm2D(16),
     Dropout(p = 0.1),
     ReLU(),
-    Conv(16, 120, 4),
-    BatchNorm(120),
+    Conv(16, 120, 5),
+    BatchNorm2D(120),
     Flatten(),
     Linear(120, 80),
     Dropout(p = 0.1),
@@ -70,4 +70,4 @@ def train(X, y, n_epochs = 200):
     print("Test accuracy: ", accuracy_score(np.argmax(y_test, axis = 1), np.argmax(y_pred, axis = 1)))
 
     
-train(X, y, 10)
+train(X, y, 30)
